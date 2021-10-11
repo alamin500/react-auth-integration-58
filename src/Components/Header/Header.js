3c592fb05d8dc50936eb0da4e49ad58c507c8848
alamin500 <nalamin500@gmail.com>
2021-10-11 20:45:05 +0600

    h

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
   return (
     <div className="header">
       <Link to="/home">Home</Link>
+      <Link to="/shipping">Shipping</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
       <span>{user.displayName}</span>
---
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route } from "react-router";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  // const {children, ...rest} = props;
+  return <Route {...rest}></Route>;
+};
+
+export default PrivateRoute;
